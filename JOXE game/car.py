3687256d0a7a93267eecbdf60539f6f4b25bb62c
Esteban Desmedt <estebandesmedt33@gmail.com>
2024-02-20 20:59:01 +0100

    small bug fix

--- JOXE game/car.py
+++ JOXE game/car.py
@@ -8,7 +8,6 @@ class Car:
         self.speed = 5  # Adjust the speed as needed
         self.image = pygame.transform.scale(pygame.image.load('./assets/resources/cars/car.png'), (grid_size, grid_size))
         self.position = self.get_start_position()
-        self.direction = 1
 
     def get_start_position(self):
         # Get the starting position based on the first road
@@ -25,26 +24,30 @@ class Car:
 
     def move_along_road(self, road):
         road_length = self.grid_size + 20  # Adjust the road length
-        road_start_x = road.x
         road_end_x = road.x + road_length
 
-        if self.direction == 1 and self.position[0] < road_end_x:
+        if self.position[0] < road_end_x:
             distance_to_move = min(self.speed, road_end_x - self.position[0])
             self.position = (self.position[0] + distance_to_move, self.position[1])
-        elif self.direction == -1 and self.position[0] > road_start_x:
-            distance_to_move = min(self.speed, self.position[0] - road_start_x)
-            self.position = (self.position[0] - distance_to_move, self.position[1])
         else:
-            # Change direction when reaching the end or the start of a road
-            self.direction *= -1
+            # Move to the next road when reaching the end of the current road
+            self.current_road_index = (self.current_road_index + 1) % len(self.road_objects)
 
-            # Update the car's y position to the y position of the next or previous road
-            if self.direction == 1:
-                self.current_road_index = (self.current_road_index + 1) % len(self.road_objects)
+            if self.current_road_index == 0:  # If the car has reached the end of the final road
+                # Reset the car's position to the start of the first road
+                first_road = self.road_objects[0]
+                self.position = (first_road.x, first_road.y)
             else:
-                self.current_road_index = (self.current_road_index - 1) % len(self.road_objects)
-            next_or_previous_road = self.road_objects[self.current_road_index]
-            self.position = (self.position[0], next_or_previous_road.y)
+                next_road = self.road_objects[self.current_road_index]  # Get the next road
+
+                # Gradually change the car's x position to the x position of the next road
+                if self.position[0] < next_road.x:
+                    self.position = (self.position[0] + 1, self.position[1])
+                elif self.position[0] > next_road.x:
+                    self.position = (self.position[0] - 1, self.position[1])
+
+                # Update the car's y position to the y position of the next road
+                self.position = (self.position[0], next_road.y)
 
     def draw(self, window):
         # Draw the car at its current position
