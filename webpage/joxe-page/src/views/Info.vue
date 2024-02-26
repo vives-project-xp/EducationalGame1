d82447c4276e9001f44ebeae8e3d6882d2065c79
Xander Vyvey <xander.vyvey@student.vives.be>
2024-02-26 14:55:18 +0100

    info-page

--- webpage/joxe-page/src/views/Info.vue
+++ webpage/joxe-page/src/views/Info.vue
@@ -3,23 +3,68 @@
 
 <template>
   <main>
+    <div class="mainInfo">
     <h1>How does it work?</h1>
+    <p>On this page you'll find more about the board and PC-game.</p>
+    </div>
+    <div class="tutorial">
     <h2>Tutorial</h2>
-    <br>
     <p>A tutorial video about the game will appear here.</p>
+    </div>
+    <div class="manual">
+    <h2>Manual</h2>
+    <p>Download the manual for the board game here.</p>
+    </div>
+    <div class="footerMenu">
+    <p>Project made by Esteban Desmedt, Jasper Vantuyne, Olivier Westerman and Xander Vyvey in 2024 for VIVES</p>
+    </div>
   </main>
 </template>
 
 <style scoped>
-    * {
-    color: white;
+    .mainInfo {
+        color: white;
+        margin-left: 20px;
+        margin-bottom: 40px;
     }
 
     h1 {
-        font-size: 70px;
+        font-size: 50px;
+        margin-bottom: 30px;
+        font-family: 'Public Pixel', sans-serif;
+    }
+
+    .tutorial {
+      background-color: #f8f4f0;
+      color: black;
+      height: 400px;
+      border-radius: 80px;
+      padding: 50px;
+      text-align: center;
+    }
+
+    h2 {
+        font-size: 30px;
+        margin-bottom: 30px;
+        font-family: 'Public Pixel', sans-serif;
+    }
+
+    .manual {
+        color: white;
+        padding: 50px;
+        text-align: center;
     }
 
     main {
         margin-top: 50px;
+    }
+
+    .footerMenu {
+    background-color: #f8f4f0;
+    color: gray;
+    border-top-left-radius: 80px;
+    border-top-right-radius: 80px;
+    text-align: center;
+    padding: 40px;
     }
 </style>
